94e091fbf6704007b19da3e695397cb2bb8b4edd
ankan1612 <ankangiri@gmail.com>
2017-10-27 12:23:34 -0400

    initial commit

--- DesignPatternsInCSharp/Properties/AssemblyInfo.cs
+++ DesignPatternsInCSharp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("63384981-9372-4d79-b638-9282b02e7e00")]
+[assembly: Guid("3c27170f-4115-41cb-acc4-647ae02e2618")]
 
 // Version information for an assembly consists of the following four values:
 //
