0ecd83965fc06a242d14a99e893d1a739bb64246
ankan1612 <ankangiri@gmail.com>
2017-10-29 03:16:31 -0400

    Adding singleton pattern

---
+++ DesignPatternsInCSharp/Singleton/MainClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Singleton
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Printer.PrinterInstance.GetDetails();
+            Printer.PrinterInstance.GetDetails();
+            Printer.PrinterInstance.GetDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternsInCSharp/Singleton/Printer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Singleton
+{
+    //This is the singleton class
+    class Printer
+    {
+        private static Printer Instance = null;
+
+        private string Name { get; set; }
+        private string IP { get; set; }
+        private string Port { get; set; }
+
+        public Printer()
+        {
+            Name = "HP Officejet";
+            IP = "192.168.1.47";
+            Port = "8080";
+        }
+
+        private static object syncLock = new object();
+
+        public static Printer PrinterInstance
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    if (Printer.Instance == null)
+                        Printer.Instance = new Printer();
+
+                    return Printer.Instance;
+                }
+            }
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Printer is : {0} running on : {1}:{2}", Name, IP, Port);
+        }
+
+
+
+    }
+}
